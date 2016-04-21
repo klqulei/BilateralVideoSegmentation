946c471f6ee7126165f4fc3ab09518887385850d
Oliver Wang <owang@adobe.com>
2016-04-20 22:50:33 -0700

    initial commit

--- sliceOld.m
+++
@@ -1,8 +0,0 @@
-% slice bilateral data from the bilateral grid
-function sliced = slice(labels,vidIndices,vidWeights)
-
-sliced = zeros(size(vidIndices,1),size(labels,2));
-for i=1:size(vidIndices,2)
-     sliced = sliced + labels(vidIndices(:,i),:) .* ...
-        repmat(double(vidWeights(:,i)),1,size(labels,2));
-end
